c5c925f729f19d4be1f5115abd20ede4e33d8f38
CDHgithubacc <collinhansen2@gmail.com>
2016-03-26 17:10:58 -0400

    More code modifications correcting sortEvents and freeTimeUntil
    put bubble sort in order
    corrected naieve and aware date time formats
    corrected if statements that didnt work
    corrected while conditional

--- eventsInCalendar.py
+++ eventsInCalendar.py
@@ -1,5 +1,6 @@
 from icalendar import Calendar
 import datetime
+import pytz
 
 class Event:
     def __init__(self, name, start, end, boolType):
@@ -22,7 +23,7 @@ class Tasks:
 cal = Calendar.from_ical(open("testUVAhackathon.ics").read())
 
 eventList = []
-tasksList = [1, 2, 3]
+taskList = [1, 2, 3]
 
 for event in cal.walk("vevent"):
         name = event.get("summary")
@@ -32,33 +33,40 @@ for event in cal.walk("vevent"):
 
 
 def sort_events():
-        for i in range(1,len(eventList)-1,1):          #lame bubblesort.py
-            for k in range(i,len(eventList)-1,1):
-                if eventList[k].start_time < eventList[k-1].start_time:
+        for i in range(len(eventList)-1,0,-1):          #lame bubblesort.py
+            for k in range(i):
+                if eventList[k].start_time > eventList[k+1].start_time:
                     temp = eventList[k]
-                    eventList[k] = eventList[k-1]
-                    eventList[k-1] = temp              #eventList are now sorted
+                    eventList[k] = eventList[k+1]
+                    eventList[k+1] = temp              #eventList are now sorted
 
 
 def free_Time_Until(dueDate):
-    currentDate = datetime.datetime.now()
+    currentDate = datetime.datetime.now(pytz.utc)
     totalTime =  dueDate - currentDate
     i = 0
-    while i < len(eventList)-2 and eventList[i].end_time < dueDate:
+    while i < len(eventList)-2 and dueDate > eventList[i].end_time:
+        print totalTime
         if eventList[i+1].start_time > dueDate:
-            if  dueDate - eventList[i].end_time <= 30:
-                totalTime = totalTime - dueDate - eventList[i].end_time
+            if  dueDate - eventList[i].end_time <= datetime.timedelta(minutes=30):
+                totalTime = totalTime - (dueDate - eventList[i].end_time)
+                print dueDate - eventList[i].end_time
             else:
                 totalTime = totalTime - datetime.timedelta(minutes=30)
+                print 30
         else:
-            if eventList[i+1].start_time - eventList[i].end_time <= 30:
+            if eventList[i+1].start_time - eventList[i].end_time <= datetime.timedelta(minutes=30):
                 totalTime = totalTime - (eventList[i+1].start_time - eventList[i].end_time)
+                print (eventList[i+1].start_time - eventList[i].end_time)
             else:
                 totalTime = totalTime - datetime.timedelta(minutes=30)
+                print  30
         totalTime = totalTime - (eventList[i].end_time - eventList[i].start_time)
+        print (eventList[i].end_time - eventList[i].start_time)
         i+=1
         if eventList[i].start_time < dueDate and eventList[i].end_time > dueDate:
-            totalTime = totalTime - (dueDste - eventList[i].start_time
+            totalTime = totalTime - (dueDate - eventList[i].start_time)
+            print dueDate - eventList[i].start_time
                                      
     return totalTime #turn this into an int?? calc an int the whole time??
 
@@ -70,4 +78,4 @@ def calculate_Priority():
 
 
 sort_events()
-print free_Time_Until(dateTime.dateTime.now+timeDelta(hours = 30))
+print free_Time_Until(datetime.datetime.now(pytz.utc)+datetime.timedelta(hours = 60))
