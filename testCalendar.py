f1de1194499642e381e372d27efef2a87d395f14
The-O-King <techmanozmar@gmail.com>
2016-03-27 04:25:14 -0400

    While Loop for Event Add
    Index out of bounds fixed

--- testCalendar.py
+++ testCalendar.py
@@ -181,7 +181,7 @@ def createDB():
     eventIndex = 0
     while eventIndex < len(eventList)-1:
         layerTwo = {'day': str(currentDate.day), 'Month': currentDate.strftime("%B"), 'Week': currentDate.strftime("%A"), "Data" : []}
-        while eventList[eventIndex].start_time.date() == currentDate:
+        while eventIndex < len(eventList)-1 and eventList[eventIndex].start_time.date() == currentDate:
             temp = eventList[eventIndex]
             layerTwo["Data"].append({"event": temp.name, "start": temp.start_time, "end": temp.end_time, "type": temp.givenType})
             eventIndex = eventIndex + 1
