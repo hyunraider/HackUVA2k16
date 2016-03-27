32e2c849abfad34bb2d397a72be1d98ddf33957c
Austin Bae <1551780@gmail.com>
2016-03-27 00:40:32 -0400

    Austins final commits. good luck

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, url_for, render_template, jsonify
+from flask import Flask, url_for, render_template, jsonify, request, json
 
 
 app = Flask(__name__)
 
+#Dummy DB that I've been testing on. Replace it with the real one!
+emptydb = []
+
+#Dummy DB That I've been using so far.
 dbs = [
 	{
 		'day': "21",
@@ -10,16 +14,16 @@ dbs = [
 		"Week": "Monday",
 		"Data": [
 			{
-				'event': "Event 1",
-				'start': 0300,
-				'end': 0500,
+				'event': "CS225",
+				'start': 8,
+				'end': 9,
 				'type': True
 			},
 			{
-				'event': "Event 2",
-				'start': 0700,
-				'end': 1000,
-				'type': True
+				'event': "Math Homework",
+				'start': 10,
+				'end': 14,
+				'type': False
 			}
 		]
 	},
@@ -27,64 +31,80 @@ dbs = [
 		'day': "22",
 		"Month": "March",
 		"Week": "Tuesday",
+		"Data": [
+		]
+	},
+	{
+		'day': "23",
+		"Month": "March",
+		"Week": "Wednesday",
+		"Data": []
+	},
+	{
+		'day': "24",
+		"Month": "March",
+		"Week": "Thursday",
 		"Data": []
 	}
 ]
 
-db2 = [
-	["21", "March", "Monday",
-		[
-			[
-				"Waking up",
-				600,
-				700,
-				True
-			],
-			[
-				"Class 1",
-				800,
-				1000,
-				True
-			],
-			[
-				"Math Assignment",
-				1200,
-				1400,
-				False
-			]
-		] 	
-	],
-	["22", "March", "Tuesday",
-		[
-			[
-				"Waking up",
-				600,
-				700,
-				True
-			],
-			[
-				"Class 3",
-				1100,
-				1200,
-				True
-			],
-			[
-				"Science Assignment",
-				1600,
-				1800,
-				False
-			]
-		] 	
-	]
-]
-
+#Just a testing route for in case you want to print out something for testing
 @app.route('/testing')
 def test():
-	return str(len(dbs));
+	return str(emptydb);
 
+#Our main route. You can connect the db here using parameters
 @app.route('/')
 def index():
-	return render_template("index.html", dbs = dbs, test=True)
+	return render_template("index.html", dbs = dbs)
 
+#This Method is just a helper function so that you can take
+#a string of Date in MM/DD/YYYY/HH/MM format and convert it into
+#an array with arr[0]=day, arr[1]=Month (got lazy we're only doing one week)
+#and arr[4]=Hour, arr[5]=Month, so you can access the array elsewhere
+def deserializedate(date):
+	quickdate = [int(date[3,5]), "March", int(date[11,13]), int(date[14,16])]
+	return quickdate
 
+#This is a post URL where the magic happens, AKA when we process the AJAX call
+#This is the way we can send frontend information (the event form) to the backend
+#so you can add to the existing Database. 
+@app.route('/addevent', methods=['POST'])
+def addevent():
+	name = request.form.get('name')
+	start = request.form.get('start')
+	end = request.form.get('end')
+	#You will need to do python magic here to insert the necessary data
+	#by first searching for the appropriate 2nd DB layer based on the month and day
+	#then inserting the day, Month, Week, and the Data into the db
+	#A lot of searching/inserting is taking place, use the deserializedate() helper
+	#function to your advantage.
+	#Collin you know this python stuff better than i do
+	#Declaring an index variable may be helpful
+	
+	#this is where you insert it into the db. Remember it is incomplete because its automatically
+	#inserting into the FIRST LAYER OF DB! Which is NOT what we want.
+	db.append({'name': name, 'start': start, 'end': end})
+
+	#leave this line to be as it is
+	return json.dumps({'status':'OK','name':name})
+
+#This is the post URL for Adding Assignments -> Same shit as before
+@app.route('/addassign', methods=['POST'])
+def addassign():
+	name = request.form.get('name')
+	start = request.form.get('start')
+	end = request.form.get('end')
+	priority = request.form.get('priority')
+	#Same thing here as the comments above
+	
+	#this is where you insert it into the db. Remember it is incomplete because its automatically
+	#inserting into the FIRST LAYER OF DB! Which is NOT what we want.
+	db.append({'name': name, 'start': start, 'end': end, 'priority': priority})
+
+	#leave this line to be as it is
+	return json.dumps({'status':'OK','name':name})
+
+#Friendly reminder that your db will erase if there is a change in the 
+#system because debug is on-> during the demo it shouldn't happen.
 app.run(debug=True, port=8000, host='0.0.0.0')
--- static/main.js
+++ static/main.js
@@ -2,3 +2,73 @@ $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
 });
+
+$("#eventadd").click(function(e) {
+	e.preventDefault();
+	$("#eventform").show();
+});
+
+$(".glyph-event").click(function(e) {
+	e.preventDefault();
+	$("#eventform").hide();
+});
+
+$("#assignadd").click(function(e) {
+	e.preventDefault();
+	$("#assignform").show();
+});
+
+$(".glyph-assign").click(function(e) {
+	e.preventDefault();
+	$("#assignform").hide();
+});
+
+$('#Abutton').click(function(e){
+		e.preventDefault();
+		console.log("starting");
+		var name = $('#AinputName').val();
+		var start = $('#AinputStart').val();
+		var end = $('#AinputEnd').val();
+		var priority = $('#Apriority').val();
+
+		$.ajax({
+			url: '/addassign',
+			data: {
+				name: name,
+				start: start,
+				end: end,
+				priority: priority
+			},
+			type: 'POST',
+			success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+          	}
+	});
+});
+
+$('#Ebutton').click(function(e){
+		e.preventDefault();
+		console.log("starting");
+		var name = $('#EinputName').val();
+		var start = $('#EinputStart').val();
+		var end = $('#EinputEnd').val();
+
+		$.ajax({
+			url: '/addevent',
+			data: {
+				name: name,
+				start: start,
+				end: end
+			},
+			type: 'POST',
+			success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+          	}
+	});
+});
